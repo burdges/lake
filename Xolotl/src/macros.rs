0260df4357b82b74c70da1a6e0f354869decd1be
Jeffrey Burdges <burdges@gnunet.org>
2017-01-03 15:29:21 +0000

    I think non-Drop types are zeroed on Drop.
    We need to write tests to verify this.

--- Xolotl/src/macros.rs
+++ Xolotl/src/macros.rs
@@ -1,15 +1,4 @@
 
-
-macro_rules! impl_KeyDrop {
-    ($t:ident) => {
-        impl Drop for $t {
-            fn drop(&mut self) {
-                for c in self.0.iter_mut() { *c = 0; }
-            }
-        }
-    }
-}
-
 // TODO: Write flexible macro for tuple structs.
 macro_rules! impl_Display_as_hex_for_WrapperStruct {
     ($t:ident) => {
@@ -20,4 +9,22 @@ macro_rules! impl_Display_as_hex_for_WrapperStruct {
         }
     }
 }
+
+
+/// Rust should zero non-`Drop` types when it drops them.  We should 
+/// verify this with a test thought.  It also useful to uncomment this
+/// ocasionally to test the build with non-`Copy` key material.
+
+macro_rules! impl_KeyDrop {
+    ($t:ident) => {
+/*
+        impl Drop for $t {
+            fn drop(&mut self) {
+                for c in self.0.iter_mut() { *c = 0; }
+            }
+        }
+*/
+    }
+}
+
 
